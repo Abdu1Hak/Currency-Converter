0f75a4f2fd0d7a2e7cc80bbb5f0aa1ca1d4e1609
Abdu1Hak <158542567+Abdu1Hak@users.noreply.github.com>
2024-12-23 17:34:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// API Stuff
+const apiKey = '111dc5a843628c7d2c8df200'
+const apiUrl = 'https://v6.exchangerate-api.com/v6'
+
+
+// Variables
+const amount = document.getElementById('amount')
+
+const currencyOne = document.getElementById('currency-one')
+const swap = document.getElementById('swap-btn')
+const currencyTwo = document.getElementById('currency-two')
+
+const exchangeRatesBtn = document.getElementById('rates-btn')
+
+const errorMessage = document.getElementById('error')
+const rateForOne = document.getElementById('rate-for-one')
+const amountIn = document.getElementById('amount-in')
+const amountOut = document.getElementById('amount-out')
+
+
+async function convertor(val) {
+
+    currency_one = currencyOne.value 
+    currency_two = currencyTwo.value
+    amount_value = amount.value
+
+    const response = await fetch(apiUrl + `/${apiKey}/latest/${currency_one}`)
+    var data = await response.json()
+    console.log(data)
+
+    // acceess the conversion rates 
+    rates = data.conversion_rates[currency_two]
+    rateForOne.innerText = `1 ${currency_one} = ${rates} ${currency_two}`
+
+
+
+    // Messages dissapear as Soon as person starts typing amount
+    amount.addEventListener('input', ()=>{ 
+        errorMessage.style.display = 'none';
+        rateForOne.style.display = 'none'
+    })
+
+    // amount validator
+    if (val == ''){ 
+        errorMessage.style.display = 'block'
+    } else if(isNaN(Number(val))) {
+        errorMessage.style.display = 'block'
+    } else{ 
+        rateForOne.style.display = 'block'
+    };
+
+    // update amount in & amount out
+    amountIn.innerHTML = `${val} ${currency_one}`
+    amountOut.innerHTML = `${(Number(val)*rates).toFixed(2)} ${currency_two}`
+}
+
+// swap functionality
+swap.addEventListener('click', () => {
+    const tempCurrency = currencyOne.value;
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = tempCurrency;
+    convertor(amount.value);
+});
+
+// main event listener
+exchangeRatesBtn.addEventListener('click', function showRate(){ 
+    convertor(amount.value)
+})
